03dcca1c22ec01378f288e717657f41e380ce6f4
NotZarifGithub <flacko.programming@gmail.com>
2023-10-15 00:51:30 +0800

    button component done

---
+++ src/components/ui/Button.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Button = ({text}) => {
+  return (
+    <button className='text-white bg-black rounded-full px-[30px] py-[5px] border-black border-4 hover:bg-white hover:text-black'>
+      <p className='font-semibold capitalize'>
+        {text}
+      </p>
+    </button>
+  )
+}
+
+export default Button
