39036e85557af69c349883acd356d85281c99b9c
NotZarifGithub <flacko.programming@gmail.com>
2023-10-16 18:34:44 +0800

    testimonials section done

--- src/components/feature/AboutUs.jsx
+++ src/components/feature/AboutUs.jsx
@@ -6,17 +6,17 @@ const AboutUs = () => {
     <section className='my-[100px] mx-[20px]'>
 
 			{/* background image */}
-      <div className="bg-[url('/heroImage.jpg')] bg-cover h-[75vh] relative">
+      <div className="bg-[url('/heroImage.jpg')] bg-cover h-[75vh] relative lg:h-[100vh]">
 
 				{/* main content */}
-				<div className="flex flex-col items-center justify-center text-center max-w-[300px] mx-auto h-full gap-4">
-					<div className="max-w-[200px]">
-						<h1 className="text-xl font-semibold">
+				<div className="flex flex-col items-center justify-center text-center max-w-[300px] mx-auto h-full gap-4 lg:max-w-[500px]">
+					<div className="max-w-[200px] lg:max-w-[400px]">
+						<h1 className="text-xl font-semibold lg:text-3xl">
 							Empowering Your Projects: Explore Our Tool Expertise
 						</h1>
 					</div>
 					<div>
-						<p className="text-sm text-black/70">
+						<p className="text-sm text-black/70 lg:text-base">
 							Discover top-quality tools and expert guidance for all your DIY and professional projects. We're your trusted source for success.
 						</p>
 					</div>
---
+++ src/components/feature/Testimonials.jsx
@@ -0,0 +1,98 @@
+"use client"
+
+import Image from "next/image"
+import { useState, useEffect } from "react"
+
+// data for testimonials 
+const testimonialsData = [
+  {
+    id: 1,
+    name: 'facebook',
+    desc: "Your tools have improved our efficiency tenfold. We couldn't imagine our projects without them",
+    image: '/facebook.svg',
+  },
+  {
+    id: 2,
+    name: 'new balance',
+    desc: "High-quality tools that meet our production needs perfectly. Your partnership is invaluable.",
+    image: '/newbalance.svg',
+  },
+  {
+    id: 3,
+    name: 'british',
+    desc: "Your tools make landscaping a breeze. We recommend them to anyone in the industry.",
+    image: '/british.svg',
+  },
+  {
+    id: 4,
+    name: 'visa',
+    desc: "Top-notch tools for precision work. Our engineers trust your products for every project.",
+    image: '/visa.svg',
+  },
+  {
+    id: 5,
+    name: 'oracle',
+    desc: "Your tools have turned our renovation projects into works of art. Outstanding quality!",
+    image: '/oracle.svg',
+  },
+  {
+    id: 6,
+    name: 'buzzfeed',
+    desc: "Reliable tools for our solar installations. They've been a game-changer for our team.",
+    image: '/buzzfeed.svg',
+  },
+  {
+    id: 7,
+    name: "pko",
+    desc: "As auto mechanics, we demand the best tools. Your products have never let us down.",
+    image: '/pko.svg',
+  },
+  {
+    id: 8,
+    name: 'tesla',
+    desc: "Your kitchen tools are essential in our culinary training. They're durable and versatile.",
+    image: '/tesla.svg',
+  },
+]
+
+const Testimonials = () => {
+
+  // state for testimonials slider
+  const [currentSlide, setCurrentSlide] = useState(0)
+  
+  // implementing useeffect to set current slide
+  useEffect(() => {
+    const interval = setInterval(
+      () => setCurrentSlide((prev) => (prev === testimonialsData.length - 1 ? 0 : prev + 1)),
+      6000
+    );
+    return () => clearInterval(interval);
+  }, []);
+
+  return (
+    <section>
+			<div className="py-[60px]">
+
+        {/* main description */}
+        <div className="max-w-[250px] mx-auto flex justify-center items-center text-center py-[50px] md:max-w-[500px]">
+          <div>
+            <h1 className="text-3xl italic font-normal lg:text-4xl">
+              "{testimonialsData[currentSlide].desc}"
+            </h1>
+          </div>
+        </div>
+
+        {/* companies logo */}
+        <div className="grid grid-cols-3 max-w-[350px] mx-auto md:flex gap-6 md:max-w-[750px] lg:max-w-[950px] py-[50px]">
+          {testimonialsData.map((item, index) => (
+            <div key={item.id} className={`relative w-full h-[80px] ${currentSlide === index ? "border-b-4 border-black " : ''}`}>
+              <Image src={item.image} className="object-contain " fill/>
+            </div>
+          ))}
+        </div>
+			</div>
+    </section>
+  )
+}
+
+export default Testimonials
