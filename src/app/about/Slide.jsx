d79a1b9d92e861de0caa7eaae61ef0af5b56f8a2
NotZarifGithub <flacko.programming@gmail.com>
2023-10-18 16:48:33 +0800

    slide section for about page added

--- src/app/about/Slide.jsx
+++ src/app/about/Slide.jsx
@@ -2,7 +2,15 @@ import React from 'react'
 
 const Slide = () => {
   return (
-    <div>Slide</div>
+    <section>
+      <div className='bg-black py-[80px] px-[40px]'>
+        <div className='flex items-center justify-center md:text-center'>
+          <h1 className='text-4xl italic font-medium leading-[60px] text-white'>
+            Here’s what we’ve accomplished together so far:
+          </h1>
+        </div>
+      </div>
+    </section>
   )
 }
 
